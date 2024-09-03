bc3a34a812243e781c6c089fb39e27610ac04049
henrixe <henrixe80@gmail.com>
2024-09-03 08:02:26 -0400

    Missão 3 - Criando a classe de Produto -> Concluída

---
+++ ComexAPI/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ComexAPI.Models;
+
+public class Produto
+{
+    public Produto(string nome, string descricao, float preco, int quantidade)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Preco = preco;
+        Quantidade = quantidade;
+    }
+
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public float Preco {  get; set; }
+    public int Quantidade { get; set; }
+}
