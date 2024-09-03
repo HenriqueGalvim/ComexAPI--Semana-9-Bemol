f5ac2a625122f3058dfd925d9b272ab80abea3f7
henrixe <henrixe80@gmail.com>
2024-09-03 08:57:29 -0400

    Missão 4 - CRUD de Produto -> Concluída

---
+++ ComexAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,53 @@
+﻿using ComexAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComexAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutoController : ControllerBase
+{
+    private static List<Produto> listaProdutos = new List<Produto>();
+    private static int id = 0;
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public void AdicionaProduto([FromBody] Produto produto)
+    {
+        Console.WriteLine("Adicionando produto");
+        produto.Id = id++;
+        Console.WriteLine(produto.Nome);
+        Console.WriteLine(produto.Descricao);
+        Console.WriteLine(produto.Preco);
+        Console.WriteLine(produto.Quantidade);
+        listaProdutos.Add(produto);
+    }
+
+    [HttpGet]
+    public IEnumerable<Produto> ListarProdutos()
+    {
+        Console.WriteLine("Listando produtos");
+        return listaProdutos;
+    }
+
+    [HttpGet("{id}")]
+    public Produto ListarProdutoPorId(int id)
+    {
+        return  listaProdutos.FirstOrDefault(filme => filme.Id == id)!;
+    } 
+
+    [HttpPut("{id}")]
+    public Produto AtualizandoProduto(int id, [FromBody] Produto produtoAtualizar) {
+        var produto = listaProdutos.FirstOrDefault(produto => produto.Id == id);
+        produto = produtoAtualizar;
+        return produto;
+    }
+
+    [HttpDelete("{id}")]
+    public List<Produto> DeletandoProduto(int id)
+    {
+        var produto = listaProdutos.FirstOrDefault(produto => produto.Id == id)!;
+        listaProdutos.Remove(produto);
+        return listaProdutos;
+    }
+}
--- ComexAPI/Models/Produto.cs
+++ ComexAPI/Models/Produto.cs
@@ -4,14 +4,16 @@ namespace ComexAPI.Models;
 
 public class Produto
 {
-    public Produto(string nome, string descricao, float preco, int quantidade)
+    public Produto(string nome, string descricao, float preco, int quantidade, int id)
     {
+        Id = id;
         Nome = nome;
         Descricao = descricao;
         Preco = preco;
         Quantidade = quantidade;
     }
 
+    public int Id { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
     public float Preco {  get; set; }
