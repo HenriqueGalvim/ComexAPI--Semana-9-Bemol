e5fc013420d74758cbfb07f0df112bebf72b0c9a
henrixe <henrixe80@gmail.com>
2024-09-03 09:12:30 -0400

    Missão 5 - Validando parâmetros recebidos -> Concluída

--- ComexAPI/Models/Produto.cs
+++ ComexAPI/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ComexAPI.Models;
 
@@ -13,9 +14,20 @@ public class Produto
         Quantidade = quantidade;
     }
 
+    [Required]
     public int Id { get; set; }
+    [Required]
+    [MaxLength(100, ErrorMessage = "O nome do produto não pode exceder 100 caracteres")]
     public string Nome { get; set; }
+    [Required]
+    [MaxLength(500, ErrorMessage = "A descrição do produto não pode exceder 500 caracteres")]
     public string Descricao { get; set; }
+
+    [Required]
+    [Range(1,999999999, ErrorMessage = "O preço deve ser maior que 0")]
     public float Preco {  get; set; }
+
+    [Required]
+    [Range(0, 999999999, ErrorMessage = "A quantidade pode ser igual ou maior que 0")]
     public int Quantidade { get; set; }
 }
